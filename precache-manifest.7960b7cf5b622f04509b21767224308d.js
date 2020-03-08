f8637ebe2a0ab421338d156a34c3e1096adcc1d7
adlylee <adityahidayat@gmail.com>
2020-03-08 09:22:28 +0800

    deploy

---
+++ precache-manifest.7960b7cf5b622f04509b21767224308d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c3a285dd444480b18f6715b7ec5b2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a252a0a42f82750acd88",
+    "url": "/static/css/main.9be77bcd.chunk.css"
+  },
+  {
+    "revision": "a82cfc64416bbf5fad3c",
+    "url": "/static/js/2.d79f9e99.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d79f9e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a252a0a42f82750acd88",
+    "url": "/static/js/main.e5540c40.chunk.js"
+  },
+  {
+    "revision": "dcc8ed75ee7fde5de9e6",
+    "url": "/static/js/runtime-main.64f83926.js"
+  }
+]);
